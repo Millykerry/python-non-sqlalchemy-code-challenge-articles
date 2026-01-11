80705b715f7d6335c930bc43b49553f2d31b24ea
millykerry <kerryom975@gmail.com>
2026-01-11 13:08:41 +0300

    Fix imports and add testable many_to_many classes

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,41 @@
-class Article:
-    def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        
-class Author:
-    def __init__(self, name):
-        self.name = name
-
-    def articles(self):
-        pass
-
-    def magazines(self):
-        pass
-
-    def add_article(self, magazine, title):
-        pass
-
-    def topic_areas(self):
-        pass
-
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        if not isinstance(name, str):
+            raise Exception("Name must be a string")
+        if len(name) < 2 or len(name) > 16:
+            raise Exception("Name must be between 2 and 16 characters")
+        if not isinstance(category, str):
+            raise Exception("Category must be a string")
+        if len(category) == 0:
+            raise Exception("Category must be longer than 0 characters")
 
-    def articles(self):
-        pass
+        self._name = name
+        self._category = category
+        self._articles = []
 
-    def contributors(self):
-        pass
+    @property
+    def name(self):
+        return self._name
 
-    def article_titles(self):
-        pass
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise Exception("Name must be a string")
+        if len(value) < 2 or len(value) > 16:
+            raise Exception("Name must be between 2 and 16 characters")
+        self._name = value
 
-    def contributing_authors(self):
-        pass
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise Exception("Category must be a string")
+        if len(value) == 0:
+            raise Exception("Category must be longer than 0 characters")
+        self._category = value
+        
+
+
